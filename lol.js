da64abd8ca857acde854f2fd89e2fb4b6e3d9ce6
Amethyst <severus1257@gmail.com>
2024-10-10 02:00:39 +0900

    selectの中身追加

--- lol.js
+++ lol.js
@@ -1,1 +1,9 @@
 ﻿
+let select = document.getElementById("champions")
+let champions = ["エイトロ", "ジグス", "スカーナー", "ブランド"]
+for (let champion of champions) {
+    let option = document.createElement("option");
+    option.text = champion;
+    option.value = champion;
+    select.appendChild(option);
+}
