2abcb9ee0bf311db4d786d4817af92dd9e04a456
Amethyst <severus1257@gmail.com>
2024-10-18 04:57:00 +0900

    クリッカーの更新

--- js/clicker/clicker.js
+++ js/clicker/clicker.js
@@ -1,5 +1,6 @@
 ﻿var ameji = 0;
 var mine = 0;
+var mine1_cost = 30;
 
 setInterval(update_counter, 50);
 setInterval(update_ameji, 1000);
@@ -7,8 +8,13 @@ setInterval(update_ameji, 1000);
 function update_counter(){
     let ameji_counter = document.getElementById("ameji_counter");
     ameji_counter.innerHTML = `${ameji} <font size="2">アメジ</font>`;
-    let mine_counter = document.getElementById("mine_counter");
-    mine_counter.innerHTML = `${mine} <font size="2">鉱山</font>`;
+    let mine1_counter = document.getElementById("mine_counter");
+    mine1_counter.innerHTML = `${mine} <font size="2">鉱山</font>`;
+}
+
+function update_mine1_cost(){
+    let mine1_button = document.getElementById("mine1");
+    mine1_button.innerHTML = `アメジ鉱山+1<br>${mine1_cost}アメジ`
 }
 
 function update_ameji(){
@@ -19,6 +25,20 @@ function add_ameji(n = 1){
     ameji += n;
 }
 
-function add_mine(n = 1){
-    mine += n;
+function add_mine(){
+    if (ameji >= mine1_cost){
+        ameji -= mine1_cost;
+        mine += 1;
+        mine1_cost *= 2;
+        update_mine1_cost();
+    }
+    else{
+        show_error();
+    }
+}
+
+function show_error(){
+    let err = document.getElementById("err");
+    err.innerHTML = ("アメジが足りません。");
+    setTimeout(function(){err.innerHTML = "";}, 2000);
 }
