01e95e4ec6be6f7827e4cea30f3589a1186ff57c
Amethyst <severus1257@gmail.com>
2024-10-20 10:35:34 +0900

    ヘッダー・フッターの表示順序を変更でレイアウト崩れを軽減

--- js/header_footer.js
+++ js/header_footer.js
@@ -1,8 +1,9 @@
 ﻿head = document.getElementsByTagName("head")[0];
 head.innerHTML += '<link href="css/header_footer.css" rel="stylesheet">';
 
-header = document.getElementsByTagName("header")[0];
-header.innerHTML = '<a href="index.html">Amethystのホームページ<a>';
-
-footer = document.getElementsByTagName("footer")[0];
-footer.innerHTML = '<a href="index.html">トップページ</a>';
+document.addEventListener("DOMContentLoaded", function() {
+    header = document.getElementsByTagName("header")[0];
+    header.innerHTML = '<a href="index.html">Amethystのホームページ</a>';
+    footer = document.getElementsByTagName("footer")[0];
+    footer.innerHTML = '<a href="index.html">トップページ</a>';
+});
